8cae70da0861cb3e21c9fb89ccb0db8a415ea0ff
Mohammed Faizuddin <mdfaizuddin15@gmail.com>
2019-01-21 20:50:53 +0530

    adds rollup node resolve plugin

--- rollup.config.js
+++ rollup.config.js
@@ -1,3 +1,4 @@
+import resolve from "rollup-plugin-node-resolve";
 export default {
   input: "index.js",
   output: {
@@ -5,5 +6,6 @@ export default {
     format: "cjs",
     // shebang for *nix
     banner: "#!/usr/bin/env node"
-  }
+  },
+  plugins: [resolve()]
 };
